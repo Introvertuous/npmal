c56681b6457eeca9acfa090022b2a876a577e55c
Chris Ryan <theramin108@gmail.com>
2019-02-08 21:22:18 -0500

    fix(distribution): do not import from outside of /src/ to keep /src/ out of /dist/

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,13 @@
 import npmal from 'commander';
 import snag from './commands/snag';
 
-import { description, version } from '../package.json';
-
-npmal.version(version).description(description);
-
 npmal
   .command('snag <name>')
   .alias('s')
   .description('snags a particular npmjs name')
   .action(snag);
 
-  npmal.parse(process.argv);
+npmal.parse(process.argv);
 
 if (npmal.args.length === 0) {
   npmal.help();
